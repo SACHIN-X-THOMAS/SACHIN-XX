cda59d8cc94cbddfe2e079eff2bc3eed831bd63a
SACHIN-X-THOMAS <161201708+SACHIN-X-THOMAS@users.noreply.github.com>
2024-03-30 10:30:23 +0530

    Update play.py

--- SACHINxSANATANIxMUSIC/utils/inline/play.py
+++ SACHINxSANATANIxMUSIC/utils/inline/play.py
@@ -34,25 +34,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "💕✄·─·─·─·─·─·─·─·─·─💗"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "💕-ˋˏ✄·─·─·─·─·─·─·─·─💗"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "💕-ˋˏ-ˋˏ✄·─·─·─·─·─·─·─💗"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─·─💗"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─💗"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─💗"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─💗"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─💗"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─💗"
     else:
-        bar = "—————————◉"
+        bar = "💕-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·💘"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -61,8 +61,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-         InlineKeyboardButton(text="🫧 ᴏᴡɴᴇʀ 🫧", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🍁 ɢʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="👻 ‌🇴𝐖𝐍𝐄𝐑 💘", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="💓 ‌🇭‌𝐄𝐋𝐏 💗", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -72,8 +72,8 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="🫧 ᴏᴡɴᴇʀ 🫧", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🍁 ɢʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="👻 ‌🇴𝐖𝐍𝐄𝐑 💘", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="💓 ‌🇭‌𝐄𝐋𝐏 💗", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
