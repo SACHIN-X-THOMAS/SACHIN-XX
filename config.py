66485bd621cfb8bd3bf072a8e92313639dc763f2
SACHIN-X-THOMAS <161201708+SACHIN-X-THOMAS@users.noreply.github.com>
2024-06-15 12:33:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/i_m_fighter")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ll4st_opII")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+M3Xx-cRATfQ2NzE1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+M3Xx-cRATfQ2NzE1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
