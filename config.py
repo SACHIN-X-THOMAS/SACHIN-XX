1cd18485c3585cf363b63df51f671f5f4c53731f
SACHIN-X-THOMAS <161201708+SACHIN-X-THOMAS@users.noreply.github.com>
2024-06-29 23:21:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SACHIN-x-THOMAS/SACHIN-SIR-JI",
+    "https://github.com/SACHIN-x-THOMAS/SACHIN-XX",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
